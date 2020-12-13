26b912cb1a06953dacd0c28b970f3bacf692fe23
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-12-13 15:57:07 +0200

    added Project schema and Project component

--- src/client.js
+++ src/client.js
@@ -3,4 +3,5 @@ import sanityClient from '@sanity/client';
 export default sanityClient({
     projectId: '58mbs2fa',
     dataset: 'production',
+    useCdn: true
 });
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,8 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import sanityClient from '../client';
+import imageUrlBuilder from '@sanity/image-url';
+import { findAllByTitle } from '@testing-library/react';
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+    return builder.image(source);
+}
+
 const SinglePost = () => {
+    const [singlePost, setSinglePost] = useState(null);
+    const { slug } = useParams();
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_slug.current == "${slug}"]){
+            title,
+            _id,
+            slug,
+            mainImage {
+                asset-> {
+                    _id,
+                    url
+                }
+            },
+            body,
+            "name": author->name,
+            "authorImage": author->image
+        }`).then((data) => setSinglePost(data[0]))
+            .catch(console.error);
+    }, [slug]);
+
+    if (!singlePost) return <div>Loading...</div>;
+
     return (
-        <div>
-            SinglePost
-        </div>
+        <main className="bg-grey-200 min-h-screen p-12">
+            <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+                <header className="relative">
+                    <div className="absolute h-full w-full flex items-center justify-center p-8">
+                        <div className="bg-white bg-opacity-75 rounded p-12">
+                            <h1 className="cursive text-3xl lg:text-6xl mb-4">
+                                {singlePost.title}
+                            </h1>
+                            <div className="flex justify-center text-gray-800">
+                                <img src={urlFor(singlePost.authorImage).url()}
+                                    alt={singlePost.name}
+                                    className="w-10 h-10 rounded-full" />
+                                <p className="cursive flex items-center pl-2 text-2xl">
+                                    {singlePost.name}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={singlePost.mainImage.asset.url}
+                        alt={singlePost.title}
+                        className="w-full object-cover rounded-t"
+                        style={{ height: "400px" }}
+
+                    />
+                </header>
+                <div>BLOCK CONTENT</div>
+            </article>
+        </main>
     );
 }
 
